f80eb73b6a6d02192e2e05bd877c3daf8f566fea
Mathilde Arnaud <mathildarnaud0598@gmail.com>
2022-11-15 15:08:02 +0100

    done without front

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #En tant qu’utilisateur, je peux lister des tâches
+  # En tant qu’utilisateur, je peux afficher les détails d’une tâche
+  # En tant qu’utilisateur, je peux ajouter une nouvelle tâche
+  # En tant qu’utilisateur, je peux éditer une tâche (la marquer comme terminée / mettre à jour le titre et les détails)
+  # En tant qu’utilisateur, je peux supprimer une tâche
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new",  as: :new_task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch  "tasks/:id",      to: "tasks#update"
+  delete "tasks/:id",      to: "tasks#destroy"
 end
